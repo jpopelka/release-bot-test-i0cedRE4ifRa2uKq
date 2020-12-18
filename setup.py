54898b06847cb272711cf3c96c4bcc8da15386d4
Jiri Popelka <jpopelka@redhat.com>
2020-12-18 14:21:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import release_bot_test
+
+setuptools.setup(
+    name="release_bot_test_i0cedRE4ifRa2uKq",
+    version=release_bot_test.__version__,
+    author="User Cont",
+    author_email="user-cont@redhat.com",
+    description="A small example package used fo release-bot testing",
+    long_description="Test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jpopelka/release-bot-test-i0cedRE4ifRa2uKq",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
